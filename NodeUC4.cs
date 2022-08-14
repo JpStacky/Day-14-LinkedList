24730c6c346db10beb18f2edd744e8864d76c519
Jp <jyotiprakashpc@outlook.com>
2022-08-14 13:47:55 +0530

    Insertion in Between

---
+++ NodeUC4.cs
@@ -0,0 +1,8 @@
+﻿namespace LinkedList
+{
+    public class NodeUC4
+    {
+        public int data;
+        public NodeUC4 next;
+    }
+}
---
+++ ProgramUC4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class ProgramUC4
+    {
+        public static void Main()
+        {
+            InsertionUC4 insertionUC4 = new InsertionUC4();
+            insertionUC4.Insertion();
+            insertionUC4.InsertionAtMiddle(30);
+        }
+        
+    }
+}
