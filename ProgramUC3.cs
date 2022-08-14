5cf9f7692e378de61c1311c870e9618fe1d323bb
Jp <jyotiprakashpc@outlook.com>
2022-08-14 13:42:06 +0530

    Append in Linked List

---
+++ ProgramUC3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class ProgramUC3
+    {
+        public static void Main()
+        {
+            NodeUC3 node = new NodeUC3();
+
+            NodeUC2 head = new NodeUC2();
+            NodeUC2 temp = head;
+
+            temp.data = 56;
+            temp.next = new NodeUC2();
+            temp = temp.next;
+
+            temp.data = 30;
+            temp.next = new NodeUC2();
+            temp = temp.next;
+
+            temp.data = 70;
+            temp.next = new NodeUC2();
+            temp = temp.next;
+        }
+    }
+}
