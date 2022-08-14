aa968da0aa681a72a131b8d5ccede78113e88510
Jp <jyotiprakashpc@outlook.com>
2022-08-14 13:39:30 +0530

    Simple Linked List

---
+++ ProgramUC1.cs
@@ -0,0 +1,13 @@
+﻿namespace LinkedList
+{
+    public class ProgramUC1
+    {
+        public static void Main()
+        { 
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(70);
+            list.AddLast(30);
+            list.AddLast(56);
+        }
+    }
+}
