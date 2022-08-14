7aacd0134be1602483fe582c6a15bef496d81917
Jp <jyotiprakashpc@outlook.com>
2022-08-14 13:40:55 +0530

    Insertion into Linked List

---
+++ NodeUC2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class NodeUC2
+    {
+        public int data;
+        public NodeUC2 next;
+    }
+}
---
+++ ProgramUC2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class ProgramUC2
+    { 
+        public static void Main()
+        {
+            NodeUC2 head = new NodeUC2();
+            NodeUC2 temp = head;
+
+            temp.data = 56;
+            temp.next = new NodeUC2();
+            temp = temp.next;
+
+            temp.data = 30;
+            temp.next = new NodeUC2();
+            temp = temp.next;
+
+            temp.data = 70;
+            temp.next = new NodeUC2();
+            temp = temp.next;
+        }
+    }
+}
